39719819c56064645668e0520b5cb7306ee63447
tmarkec <tmarkec@gmail.com>
2023-01-04 14:21:21 +0000

    add background colour for right and wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,25 @@ startBtn.addEventListener('click', function() {
 }) */
 
 // game page
-let randomNum = Math.trunc(Math.random() * 1) + 1;
-console.log(randomNum);
+let randomNum = Math.trunc(Math.random() * 10) + 1;
+document.getElementById('random-pick').textContent = randomNum;
 // document.querySelector('.random-number').textContent = 10;
 // document.querySelector('.prediction-number').value = 5;
 //check button
-document.querySelector('.btn-check').addEventListener('click', function() {
-    let guess = +(document.querySelector('.prediction-number').value)
- if(guess === randomNum) {
-    document.querySelector('.random-text').textContent = 'Well done'
-    document.querySelector('.random-number').textContent = randomNum;
- }
- else if(guess > randomNum) {
-    document.querySelector('.random-text').textContent = 'Way to high'
- }
- else if(guess < randomNum) {
-    document.querySelector('.random-text').textContent = 'Way to low'
- }
+document.querySelector('.btn-check').addEventListener('click', function () {
+    let input = +(document.querySelector('.prediction-number').value)
+    if (!input) {
+        document.getElementById('text').textContent = 'Please type number!!'
+    } else if (input === randomNum) {
+        document.getElementById('text').textContent = 'Well done'
+        document.getElementById('random-pick').textContent = randomNum;
+        document.body.style.background= 'green'
+    } else if (input > randomNum) {
+        document.getElementById('text').textContent = 'Way to high'
+        document.body.style.background= 'red'
+    } else if (input < randomNum) {
+        document.querySelector('.random-text').textContent = 'Way to low'
+        document.body.style.background= 'red'
+    }
 
 })
