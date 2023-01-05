24cb6ac0c84f5d76eb1205841863ee560ffe5ed0
tmarkec <tmarkec@gmail.com>
2023-01-05 15:23:15 +0000

    adding comments to js & tidying code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,35 @@
-/**
- * landing page btn not working so it set to hidden
- * fix issue!!!!!!
-*/
+//loading page
 let games = document.getElementById("game-box");
 let startBtn = document.getElementById("start-btn");
-
+//button hides page with rules
 startBtn.addEventListener("click", function () {
   games.classList.remove("hidden");
 });
 
-// game page
-let randomNum = Math.trunc(Math.random() * 20) + 1;
+// rundom number game page
+let randomNum = Math.trunc(Math.random() * 20) + 1;//creates random number
 document.getElementById('random-pick').textContent = randomNum;
-let score = document.getElementById('nmb-blue').textContent = 0;
-let info = [];
+let score = document.getElementById('nmb-blue').textContent = 0;//score set to 0
+let info = [];//array using to display users guess numbers
 let displayText = function(text) {
     document.getElementById('text').textContent = text;
 }
-//click button to check score
+
+//function for checking user input
 document.getElementById('btn-check').addEventListener('click', function () {
     let input = +(document.getElementById('prediction-number').value);
     info.push(input);
     document.getElementById('nmb-red').textContent = info
-    
-    //guess is letter or no number
+    document.getElementById('prediction-number').value=''
+    document.getElementById('prediction-number').focus();
+    //is user guess is letter or no number
     if (!input) {
         displayText('Please type a number!!!')
 
     }
-    //guess is equal to random number
+    //guess is equal to random number-if user wins
     else if (input === randomNum) {
-        displayText= (`Well done! Your No. of guesses = ${score+1}`)
+        displayText(`Well done! Your No. of guesses = ${score+1}`)
         document.getElementById('random-pick').textContent = randomNum;
         score = score + 1;
         document.getElementById('nmb-blue').textContent = score
@@ -42,9 +41,24 @@ document.getElementById('btn-check').addEventListener('click', function () {
     }
    
     //guess is larger than random number
-    else if (input > randomNum) {
+    // else if (input > randomNum) {
+    //     if (score <= 4) {
+    //         displayText('Way to high')
+    //         score = score + 1;
+    //         document.getElementById('nmb-blue').textContent = score
+    //     } else {
+    //         displayText('You lost')
+    //         document.getElementById('prediction-number').value =''
+    //         document.getElementById('random-pick').textContent = randomNum;
+    //         document.getElementById('random-pick').style.border = '0'
+    //         document.getElementById('random-pick').style.color = 'red' 
+    //     }
+    
+    // }
+    //if user guess is lower or greater than random number
+    else if (input !== randomNum) {
         if (score <= 4) {
-            displayText('Way to high')
+            document.getElementById('text').textContent = input > randomNum ? 'Your guess is too high' : 'Your guess is too low';
             score = score + 1;
             document.getElementById('nmb-blue').textContent = score
         } else {
@@ -54,29 +68,31 @@ document.getElementById('btn-check').addEventListener('click', function () {
             document.getElementById('random-pick').style.border = '0'
             document.getElementById('random-pick').style.color = 'red' 
         }
-        
+    
     }
-   
     //guess is lower than random number
-    else if (input < randomNum) {
-        if (score <= 4) {
-            displayText('Way to low')
-            score = score + 1;
-            document.getElementById('nmb-blue').textContent = score
-        } else {
-            displayText('You lost')
-            document.getElementById('prediction-number').value ='' 
-            document.getElementById('random-pick').textContent = randomNum; 
-            document.getElementById('random-pick').style.border = '0'
-            document.getElementById('random-pick').style.color = 'red' 
-        }
-    } else {
-        document.getElementById('random-text').textContent = 'You lost'
+    // else if (input < randomNum) {
+    //     if (score <= 4) {
+    //         displayText('Way to low')
+    //         score = score + 1;
+    //         document.getElementById('nmb-blue').textContent = score
+    //     } else {
+    //         displayText('You lost')
+    //         document.getElementById('prediction-number').value ='' 
+    //         document.getElementById('random-pick').textContent = randomNum; 
+    //         document.getElementById('random-pick').style.border = '0'
+    //         document.getElementById('random-pick').style.color = 'red' 
+    //     }
+
+     else {
+        displayText('You lost')
         document.getElementById('nmb-blue').textContent = 0;
+        document.getElementById('prediction-number').value =''
+        document.getElementById('random-pick').textContent = randomNum;
     }
 });
 
-//play again game
+//function for playing another game
 document.getElementById('btn-again').addEventListener('click', function () {
     score = document.getElementById('nmb-blue').textContent = 0;
     randomNum = Math.trunc(Math.random() * 20) + 1;
